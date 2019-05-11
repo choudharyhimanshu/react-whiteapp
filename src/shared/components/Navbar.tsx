626983bfb4960076d19e3c2209752077ff08a359
Himanshu Choudhary <himanshuchoudhary@live.com>
2019-05-11 22:36:42 +0530

    Integrated login with GlobalContext & AuthService methods

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Icon, Menu } from 'semantic-ui-react';
 
+import logo from '../../logo.svg';
+
 export default class Navbar extends React.Component {
 
     render() {
         return (
             <Menu fixed='top'>
-                <Container>
+                <Container fluid>
+                    <Menu.Item className='item'><img src={logo}/><strong>FOOBAR</strong></Menu.Item>
                     <Link className='item' to='/'>Home</Link>
                     <Link className='item' to='/example'>Example</Link>
                     <Menu.Item position='right'>
---
+++ src/shared/services/auth.service.ts
@@ -0,0 +1,21 @@
+import {UserInfo} from '../models/UserInfo';
+import {CACHE_KEYS} from '../../constants';
+
+class AuthService {
+
+    async getUserInfo(): Promise<UserInfo> {
+        const userInfoCache = sessionStorage.getItem(CACHE_KEYS.USER_INFO);
+        if (userInfoCache) {
+            return Promise.resolve(JSON.parse(userInfoCache));
+        }
+        return Promise.reject('User Info not found.');
+    }
+
+    async setUserInfo(userInfo: UserInfo): Promise<string> {
+        sessionStorage.setItem(CACHE_KEYS.USER_INFO, JSON.stringify(userInfo));
+        return Promise.resolve('Login Successful.');
+    }
+
+}
+
+export default new AuthService();
